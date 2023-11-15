f25486ac02f42dcf2e565afaf3857c69828364f6
Aggelos <aggelos.almouti@gmail.com>
2023-11-15 16:56:26 +0200

    Finish input field interaction except from password authentication

---
+++ app.js
@@ -0,0 +1,8 @@
+password = document.querySelector("password");
+confirm_password = document.querySelector("confirm_password");
+button = document.querySelector("button");
+
+button.addEventListener('click', (e) => {
+    // if pass content don't match, add error class to passwords
+    // and error message
+})
